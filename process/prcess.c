f9c4924afca0d715c1c47c68a5c6729a80a59484
hungmanh150602 <zhungmanh2002@gmail.com>
2026-08-26 21:53:47 +0700

    1

---
+++ process/prcess.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void action()
+{
+    printf("do the first action\n");
+    printf("do the second action\n");
+    return;
+}
+
+int main()
+{
+    printf("I am new program.  Goodbyte!\n");
+    getchar();
+    return 123;
+}
---
+++ process/readme.md
@@ -0,0 +1,6 @@
+`ps -p 18511 -o pid,ppid,pgid,sid,cmd`
+
+```
+    PID    PPID    PGID     SID CMD
+  20574   19783   20574   19783 ./test
+```
